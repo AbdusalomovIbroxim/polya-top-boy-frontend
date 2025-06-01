bf0bff0e96d794ebdc5c5bfd7b051268bfc7a8f2
Ibroxim <ibrohimlololowka@gmail.com>
2025-06-01 19:01:30 +0500

    remove: удалил яндекс карту

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex items-center bg-white p-6 pb-2 justify-between">
-    <h2 class="text-[#131711] text-lg font-bold leading-tight tracking-[-0.015em] flex-1 text-center pl-12">Football Field</h2>
+    <h2 class="text-[#131711] text-lg font-bold leading-tight tracking-[-0.015em] flex-1 text-center">Football Field</h2>
     <!-- <div class="flex w-12 items-center justify-end">
       <button class="flex max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-12 bg-transparent text-[#131711] gap-2 text-base font-bold leading-normal tracking-[0.015em] min-w-0 p-0">
         <div class="text-[#131711]">
