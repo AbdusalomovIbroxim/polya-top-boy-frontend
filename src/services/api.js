90a4058f1a97da8ec989b3f325c188f7faf88e75
Ibroxim <ibrohimlololowka@gmail.com>
2025-06-11 14:00:52 +0500

    update: обновил страницу стадионов

--- src/services/api.js
+++ src/services/api.js
@@ -74,7 +74,6 @@ api.interceptors.response.use(
         localStorage.removeItem('access_token')
         localStorage.removeItem('refresh_token')
         localStorage.removeItem('user')
-        window.location.href = '/auth'
         return Promise.reject(refreshError)
       }
     }
