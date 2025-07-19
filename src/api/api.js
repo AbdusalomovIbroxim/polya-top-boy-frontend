a19a5d8e787fa7a228ad37d6e1282f1632332c47
AbdusalomovIbroxim <ibrohimlololowka@gmail.com>
2025-07-19 21:09:51 +0500

    update: логи для telegram initData

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://10.12.0.191:8000/api/',
+  baseURL: 'https://polya-top-bot-backend.onrender.com/api/',
   headers: {
     'Content-Type': 'application/json'
   }
