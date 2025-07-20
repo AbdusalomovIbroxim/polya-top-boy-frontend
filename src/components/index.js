533cc6672a88e814946379cf8da7f69c33038a36
AbdusalomovIbroxim <ibrohimlololowka@gmail.com>
2025-07-20 14:16:59 +0500

    update: filter pop ups

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 // Export all components for easier imports
 export { default as Navbar } from './Navbar.vue'
-export { default as FiltersRow } from './FiltersRow.vue'
 export { default as StadiumCard } from './StadiumCard.vue'
 export { default as StadiumList } from './StadiumList.vue'
 export { default as FilterDropdown } from './FilterDropdown.vue'
