7026419919a76b7efa8d1c2a85b987296ebcd9f2
AbdusalomovIbroxim <ibrohimlololowka@gmail.com>
2025-07-21 17:09:54 +0500

    update: del filters row

--- src/components/StadiumCard.vue
+++ src/components/StadiumCard.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="stadium-card" @click="$emit('click', stadium)">
-    <div class="stadium-slider">
-      <template v-if="images.length > 0">
+    <div class="stadium-slider" @touchstart="onTouchStart" @touchmove="onTouchMove" @touchend="onTouchEnd">
+      <button v-if="images.length > 1" class="slider-arrow left" @click.stop="prevImage">&#8592;</button>
+      <div class="slider-img-wrap">
         <img
-          v-for="(img, idx) in images"
-          :key="idx"
-          :src="img.image || ''"
+          v-if="images.length > 0"
+          :src="images[currentImage]?.image || ''"
           class="slider-img"
-          :alt="`${stadium.name} photo ${idx + 1}`"
+          :alt="`${stadium.name} photo ${currentImage + 1}`"
         />
-      </template>
-      <div v-else class="slider-img slider-img-placeholder">Нет фото</div>
+        <div v-else class="slider-img slider-img-placeholder">Нет фото</div>
+      </div>
+      <button v-if="images.length > 1" class="slider-arrow right" @click.stop="nextImage">&#8594;</button>
     </div>
     <div class="stadium-content">
       <p class="stadium-title">{{ stadium.name }}</p>
@@ -41,6 +42,13 @@ export default {
     }
   },
   emits: ['click', 'open'],
+  data() {
+    return {
+      currentImage: 0,
+      touchStartX: 0,
+      touchEndX: 0
+    }
+  },
   computed: {
     images() {
       return Array.isArray(this.stadium.images) ? this.stadium.images : [];
@@ -51,6 +59,40 @@ export default {
       if (!price) return '-';
       const num = Math.round(Number(price) * 3000);
       return num.toLocaleString('ru-RU');
+    },
+    prevImage(e) {
+      e && e.stopPropagation();
+      if (this.currentImage > 0) {
+        this.currentImage--;
+      } else {
+        this.currentImage = this.images.length - 1;
+      }
+    },
+    nextImage(e) {
+      e && e.stopPropagation();
+      if (this.currentImage < this.images.length - 1) {
+        this.currentImage++;
+      } else {
+        this.currentImage = 0;
+      }
+    },
+    onTouchStart(e) {
+      this.touchStartX = e.touches[0].clientX;
+    },
+    onTouchMove(e) {
+      this.touchEndX = e.touches[0].clientX;
+    },
+    onTouchEnd() {
+      const dx = this.touchEndX - this.touchStartX;
+      if (Math.abs(dx) > 40) {
+        if (dx < 0) {
+          this.nextImage();
+        } else {
+          this.prevImage();
+        }
+      }
+      this.touchStartX = 0;
+      this.touchEndX = 0;
     }
   }
 }
