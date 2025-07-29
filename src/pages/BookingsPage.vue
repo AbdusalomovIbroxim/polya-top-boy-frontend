06d6b605526b3980d8093be454addb0c1f6fe783
AbdusalomovIbroxim <ibrohimlololowka@gmail.com>
2025-07-29 16:32:02 +0500

    Обновлен UI страницы истории бронирований с современным дизайном

--- src/pages/BookingsPage.vue
+++ src/pages/BookingsPage.vue
@@ -12,11 +12,11 @@
     
     <!-- Header -->
     <div class="bookings-header">
-      <button class="back-button" @click="goBack">
+      <!-- <button class="back-button" @click="goBack">
         <svg width="24" height="24" viewBox="0 0 256 256" fill="currentColor">
           <path d="M224,128a8,8,0,0,1-8,8H59.31l58.35,58.34a8,8,0,0,1-11.32,11.32l-72-72a8,8,0,0,1,0-11.32l72-72a8,8,0,0,1,11.32,11.32L59.31,120H216A8,8,0,0,1,224,128Z"></path>
         </svg>
-      </button>
+      </button> -->
       <h2 class="bookings-title">Мои бронирования</h2>
     </div>
 
